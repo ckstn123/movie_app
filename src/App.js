966bbda77d6f6249fc4f93075332af90ba7f20dc
chansu <whckstn12@naver.com>
2020-05-27 00:28:16 +0900

    jsx, propTypes 학습

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
 
+const foodILike = [
+    {
+        id:1,
+        name : "kimchi",
+        rating : 5
+    },
+    {
+        id:2,
+        name : "ramen",
+        rating : 4.9
+    },
+    {
+        id:3,
+        name : "삽겹살",
+        rating : 4.8
+    },
+    {
+        id:4,
+        name : "불고기",
+        rating : 4.7
+    }];
+
+function renderFood(dish) {
+    return <Food favourite={dish.name}/>
+}
+function Food({favourite, rating}) {
+
+    return (
+        <div>
+            <h1>I love {favourite}</h1>
+            <h4>{rating}/5.0</h4>
+        </div>
+    );
+}
+
+Food.propTypes = {
+    name: PropTypes.string.isRequired,
+    rating: PropTypes.number
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {foodILike.map(dish => (
+            <Food key = {dish.id} favourite = {dish.name + "♥"} rating = {dish.rating}/>)
+        )}
+
     </div>
   );
 }
